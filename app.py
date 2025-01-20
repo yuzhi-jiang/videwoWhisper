3caf58b181971e2499237c123274505fff01c078
yuzhi <1226325909@qq.com>
2025-01-20 11:38:08 +0800

    update host 0.0.0.0
    设置所有人可访问

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def upload_file():
         return jsonify({'error': f'处理失败: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    app.run(debug=False, port=5000,host="0.0.0.0") 
